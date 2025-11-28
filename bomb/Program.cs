32cd7e4ca587fe661c5280f85446d8e4d0cdd194
MSI\boltk <boltkun.piku@gmail.com>
2025-11-28 13:34:59 +0900

    テスト

--- bomb/Program.cs
+++ bomb/Program.cs
@@ -17,6 +17,9 @@ namespace bomb
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.WriteLine("tamesi");
         }
     }
+   
 }
