e0cbc4d5267b3d6ed7ee1734c8abb63c333cfc6b
MSI\hiro2 <zeal25360012@fsgcl.jp>
2025-12-02 10:22:34 +0900

    何か

--- bomb/Form1.cs
+++ bomb/Form1.cs
@@ -11,24 +11,6 @@ using bomb.bomb;
 
 namespace bomb
 {
-<<<<<<< HEAD
-    public partial class Form1 : Form
-    {
-        private Timer gameTimer;
-        private GameBoard board;
-
-        public Form1()
-        {
-            InitializeComponent();
-            this.Load += Form1_Load; // ← これが必要！
-            this.KeyPreview = true;
-            this.KeyDown += Form1_KeyDown;
-
-            gameTimer = new Timer();
-            gameTimer.Interval = 100;
-            gameTimer.Tick += GameTimer_Tick;
-            gameTimer.Start();
-=======
    
     public partial class Form1 : Form
         {
@@ -83,44 +65,9 @@ namespace bomb
             if (e.KeyCode == Keys.Space) board.PlaceBomb();
 
             Invalidate(); // 再描画
->>>>>>> origin/master
         }
     }
 
 
 
-<<<<<<< HEAD
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            board = new GameBoard(15, 15);
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Up) board.Player.Move(0, -1);
-            if (e.KeyCode == Keys.Down) board.Player.Move(0, 1);
-            if (e.KeyCode == Keys.Left) board.Player.Move(-1, 0);
-            if (e.KeyCode == Keys.Right) board.Player.Move(1, 0);
-            if (e.KeyCode == Keys.Space) board.PlaceBomb();
-
-            this.Invalidate();
-        }
-
-        private void GameTimer_Tick(object sender, EventArgs e)
-        {
-            board.Update();
-            this.Invalidate();
-        }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            if (board != null)
-            {
-                board.Draw(e.Graphics);
-            }
-        }
-    }
-=======
->>>>>>> origin/master
 }
