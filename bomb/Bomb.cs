f8d1ea16fc87b8d05489f3c7070297996b97851b
MSI\boltk <boltkun.piku@gmail.com>
2025-12-05 15:51:57 +0900

    障害物を追加しました

--- bomb/Bomb.cs
+++ bomb/Bomb.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 
 
+
 namespace bomb
 {
     public class Bomb
@@ -26,9 +27,38 @@ namespace bomb
             return timer <= 0; // trueなら爆発終了
         }
 
+        // 爆発範囲を返す
+        public List<Point> Explode(int range = 2)
+        {
+            List<Point> blast = new List<Point>();
+
+            // 爆心地
+            blast.Add(new Point(X, Y));
+
+            // 上下左右に広げる
+            for (int i = 1; i <= range; i++)
+            {
+                blast.Add(new Point(X + i, Y)); // 右
+                blast.Add(new Point(X - i, Y)); // 左
+                blast.Add(new Point(X, Y + i)); // 下
+                blast.Add(new Point(X, Y - i)); // 上
+            }
+
+            return blast;
+        }
+
         public void Draw(Graphics g, int cellSize)
         {
             g.FillEllipse(Brushes.Red, X * cellSize, Y * cellSize, cellSize, cellSize);
+        }
+
+        // 爆風を描画（黄色）
+        public void DrawBlast(Graphics g, int cellSize, List<Point> blast)
+        {
+            foreach (var p in blast)
+            {
+                g.FillRectangle(Brushes.Yellow, p.X * cellSize, p.Y * cellSize, cellSize, cellSize);
+            }
         }
     }
 }
