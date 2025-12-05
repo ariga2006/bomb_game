6a7f6b35ceca38b133c48a9294ae732eba9f41cc
hisui <yuukobi0403@gmail.com>
2025-12-05 16:41:13 +0900

    マージ

--- bomb/Form1.cs
+++ bomb/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 namespace bomb
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
     public partial class Form1 : Form
@@ -30,6 +31,8 @@ namespace bomb
             gameTimer.Tick += GameTimer_Tick;
             gameTimer.Start();
 =======
+=======
+>>>>>>> 4b5cbbc (衝突消し)
    
 >>>>>>> 0549bb0 (no message)
     public partial class Form1 : Form
@@ -39,16 +42,22 @@ namespace bomb
 
         public Form1()
         {
+<<<<<<< HEAD
             InitializeComponent();
             this.DoubleBuffered = true;
             this.KeyPreview = true;
             this.KeyDown += Form1_KeyDown;
+=======
+            private Timer gameTimer;
+            private GameBoard board;
+>>>>>>> 4b5cbbc (衝突消し)
 
             gameTimer = new Timer();
             gameTimer.Interval = 100;
             gameTimer.Tick += GameTimer_Tick;
             gameTimer.Start();
 
+<<<<<<< HEAD
             board = new GameBoard(20,20); // 15×15の盤面
         }
         
@@ -88,13 +97,27 @@ namespace bomb
         {
             board = new GameBoard(15, 15);
         }
+=======
+                gameTimer = new Timer();
+                gameTimer.Interval = 100; // 0.1秒ごとに更新
+                gameTimer.Tick += GameTimer_Tick;
+                gameTimer.Start();
+
+             board = new GameBoard(20, 20);//15×15の盤面
+            }
+
+       
+>>>>>>> 4b5cbbc (衝突消し)
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            //矢印キーで移動
             if (e.KeyCode == Keys.Up) board.Player.Move(0, -1);
             if (e.KeyCode == Keys.Down) board.Player.Move(0, 1);
             if (e.KeyCode == Keys.Left) board.Player.Move(-1, 0);
             if (e.KeyCode == Keys.Right) board.Player.Move(1, 0);
+
+            //スペースで爆弾設置
             if (e.KeyCode == Keys.Space) board.PlaceBomb();
 
             this.Invalidate();
@@ -117,9 +140,13 @@ namespace bomb
         }
     }
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 =======
 =======
 >>>>>>> origin/master
+=======
+
+>>>>>>> 4b5cbbc (衝突消し)
 }
 >>>>>>> 0549bb0 (no message)
