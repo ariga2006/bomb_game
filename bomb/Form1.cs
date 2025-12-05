b0300f389e4bf7eaa09d517477e618a27d0a36a7
hisui <yuukobi0403@gmail.com>
2025-12-05 16:36:15 +0900

    マージ

--- bomb/Form1.cs
+++ bomb/Form1.cs
@@ -10,6 +10,28 @@ using System.Windows.Forms;
 
 namespace bomb
 {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+    public partial class Form1 : Form
+    {
+        private Timer gameTimer;
+        private GameBoard board;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load; // ← これが必要！
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+
+            gameTimer = new Timer();
+            gameTimer.Interval = 100;
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+=======
+   
+>>>>>>> 0549bb0 (no message)
     public partial class Form1 : Form
     {
         private Timer gameTimer;
@@ -48,6 +70,7 @@ namespace bomb
             // スペースで爆弾設置
             if (e.KeyCode == Keys.Space) board.PlaceBomb();
 
+<<<<<<< HEAD
             Invalidate();
         }
 
@@ -55,6 +78,33 @@ namespace bomb
         {
             board.Update();
             Invalidate();
+=======
+            Invalidate(); // 再描画
+>>>>>>> origin/master
+        }
+
+<<<<<<< HEAD
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board = new GameBoard(15, 15);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) board.Player.Move(0, -1);
+            if (e.KeyCode == Keys.Down) board.Player.Move(0, 1);
+            if (e.KeyCode == Keys.Left) board.Player.Move(-1, 0);
+            if (e.KeyCode == Keys.Right) board.Player.Move(1, 0);
+            if (e.KeyCode == Keys.Space) board.PlaceBomb();
+
+            this.Invalidate();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            board.Update();
+            this.Invalidate();
+>>>>>>> 0549bb0 (no message)
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -66,4 +116,10 @@ namespace bomb
             }
         }
     }
+<<<<<<< HEAD
 }
+=======
+=======
+>>>>>>> origin/master
+}
+>>>>>>> 0549bb0 (no message)
